604b3d69a4f24bd92074d2768daa2cc20975d705
Adrian van Eeden <adrianvaneeden@gmail.com>
2017-11-19 11:06:03 +0200

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -15,46 +17,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the Chat App',
-        createAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app.'));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createAt: new Date().getTime()
-    });
-    // socket.emit('newEmail', {
-    //     from: "adrian@example.com",
-    //     text: "Hey, wasssup?",
-    //     createAt: 123
-    // });
-
-    // socket.emit('newMessage', {
-    //     from: 'Adrian',
-    //     text: 'See you later',
-    //     createdAt: 1234
-    // });
-
+    socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
+    
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
-            from: message.from,
-            text:message.text,
-            createdAt: new Date().getTime()
-        });
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: messaget.text,
-        //     createdAt: new Date().getTime()
-        // });
+        io.emit('newMessage', generateMessage(message.from, message.text));
     });
-
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail);
-    // });
 
     socket.on('disconnect', () => {
         console.log('User was disconnected from server');
